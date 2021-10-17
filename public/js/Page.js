45ac4ac2ac10b2e767115a103bd1ea89e1c8aa57
Claudia Font <claudiafontmarques@gmail.com>
2021-10-18 00:36:21 +0200

    Added counter and start and end buttons

--- public/js/Page.js
+++ public/js/Page.js
@@ -3,6 +3,10 @@ import Component from "./Component.js";
 import GetServices from "./PokemonServices.js";
 
 let offset = 0;
+let totalCount = 898;
+let first = 0;
+let last = 12;
+
 class Page extends Component {
   parentElement;
   urlApi;
@@ -28,36 +32,44 @@ class Page extends Component {
     const nextPage = document.querySelector(".next");
     const beforePage = document.querySelector(".before");
     const allPage = document.querySelector(".all");
+    const startPage = document.querySelector(".start");
+    const endPage = document.querySelector(".end");
 
     //buttons function
     nextPage.addEventListener("click", next);
     beforePage.addEventListener("click", before);
     allPage.addEventListener("click", all);
+    startPage.addEventListener("click", start);
+    endPage.addEventListener("click", end);
 
     function next() {
-      console.log("hey");
-
       document.querySelector(".container").remove();
       offset += 12;
+      first += 12;
+      last += 12;
+
       new Page(
         body,
         `https://pokeapi.co/api/v2/pokemon?limit=12&offset=${offset}`
       );
-      console.log(offset);
+
       return offset;
     }
 
     function before() {
-      console.log("hola");
       document.querySelector(".container").remove();
       if (offset === 0) {
         offset = 0;
+        first = 0;
+        last = 12;
         new Page(
           body,
           `https://pokeapi.co/api/v2/pokemon?limit=12&offset=${offset}`
         );
       } else {
         offset -= 12;
+        first -= 12;
+        last -= 12;
         new Page(
           body,
           `https://pokeapi.co/api/v2/pokemon?limit=12&offset=${offset}`
@@ -67,8 +79,34 @@ class Page extends Component {
     }
 
     function all() {
+      fisrt = 0;
+      last = 898;
       body.removeChild(document.querySelector(".container"));
       new Page(body, `https://pokeapi.co/api/v2/pokemon?limit=898&offset=0`);
+    }
+
+    function start() {
+      document.querySelector(".container").remove();
+      offset = 0;
+      first = 0;
+      last = 12;
+      new Page(
+        body,
+        `https://pokeapi.co/api/v2/pokemon?limit=12&offset=${offset}`
+      );
+
+      return offset;
+    }
+    function end() {
+      document.querySelector(".container").remove();
+      offset = 888;
+      first = 889;
+      last = 898;
+      new Page(
+        body,
+        `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`
+      );
+      return offset;
     }
   }
 
@@ -87,11 +125,20 @@ class Page extends Component {
       </header>
 
       <main>
-        <div>
+        <div> 
           <div class="button-page">
-            <button type="submit" class="all">All</button>
-            <button type="submit" class="before"><</button>
-            <button type="submit" class="next">></button>
+           <div> 
+            <button type="submit" class="all">All Pokemons</button>
+           </div> 
+           <div> 
+            <button type="submit" class="start"><<<</button>
+            <button type="submit" class="before">< Before</button>
+            <button type="submit" class="next">Next ></button>
+            <button type="submit" class="end">>>></button>
+           </div>
+           <div> 
+            <p>${first} - ${last} of 898</p>
+           </div>
           </div>
           <div class="list">
             <ul class="list-cards">
